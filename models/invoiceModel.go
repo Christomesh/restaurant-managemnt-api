ad1e95fa890a8c29c5d5789ea0f751e32d2d387d
Meshach Odejide <meshacholuwadamilare@gmail.com>
2022-06-10 23:50:48 +0100

    changed order_id datatype

--- models/invoiceModel.go
+++ models/invoiceModel.go
@@ -9,7 +9,7 @@ import (
 type Invoice struct {
 	ID               primitive.ObjectID `bson:"_id"`
 	Invoice_id       string             `json:"invoive_id"`
-	Order_id         float64            `json:"order_id"`
+	Order_id         string             `json:"order_id"`
 	Payment_method   *string            `json:"payment_method" validate:"eq=CAD|eq=CASH"`
 	Payment_status   *string            `json:"payment_status" validate:"eq=PENDING|eq=PAID"`
 	Payment_due_date time.Time          `json:"payment_due_date"`
